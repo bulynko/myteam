73d46836c08dbbe5e9874f60176e5f1de93942f6
root <marcin@bulynko.pl>
2015-12-09 16:39:42 +0400

    First commit

--- client/myteam/controllers/myteam.ng.js
+++ client/myteam/controllers/myteam.ng.js
@@ -2,6 +2,14 @@ angular.module("myteam").controller("myteamCtrl", ['$scope', '$meteor', '$rootSc
   function($scope, $meteor, $rootScope, $state, $location){
 
 
+   $meteor.session('projectDaysSession').bind($scope, 'mycounterD');
+   $meteor.session('projectHrsSession').bind($scope, 'mycounterH');
+   $meteor.session('projectMinsSession').bind($scope, 'mycounterM');
+   $meteor.session('projectSecsSession').bind($scope, 'mycounterS');
+
+    //projectTimecounter= moment(new Date());;
+    projectTimecounter= moment('2015-12-26');; 
+    Meteor.setInterval(Meteor.myFunctions.mytimer, 1000 );
 
     $scope.myteam = $meteor.collection(MyTeam).subscribe('myteam');
     $scope.teams = $meteor.collection(Teams).subscribe('teams');
@@ -16,15 +24,15 @@ angular.module("myteam").controller("myteamCtrl", ['$scope', '$meteor', '$rootSc
  //  $scope.t2={ name  : 'Cloud Design' , status : '10%'  };
  // t3={ name  : 'Instillation and Configuration' , gameType : '0%'  };
  
-   $scope.u1={ name  : 'Marcin' , contact : '050-7483280' , role : 'Tech Lead' };
-   $scope.u2={ name  : 'Sheniyas' , contact : '050-1213838' , role : 'Linux'  };    
-   $scope.u3={ name  : 'Suraj' , contact : '055-3323342', role : ''   };    
-   $scope.u4={ name  : 'Adnan' , contact : '050-1291428' , role : 'Networking'  };  
-   $scope.u5={ name  : 'Nooruddin' , contact : '050-.....' , role : 'IBM Cloud'  };
-   $scope.u6={ name  : 'Vijay' , contact : '052-5790357' , role : 'Open Stack'  };
+   $scope.u1={ name  : 'Marcin' , contact : '050-7483280' , role : 'Tech Lead' ,logo: "img/openstack.jpg"  };
+   $scope.u2={ name  : 'Sheniyas' , contact : '050-1213838' , role : 'Linux' ,logo: "img/red-hat-logo.jpg"  };    
+   $scope.u3={ name  : 'Suraj' , contact : '055-3323342', role : '' , logo: "img/login-page-logo.png"  };    
+   $scope.u4={ name  : 'Adnan' , contact : '050-1291428' , role : 'Networking' ,logo: "img/cisco.png"  };  
+   $scope.u5={ name  : 'Nooruddin' , contact : '050-0000000' , role : 'IBM Cloud' ,logo: "img/ibm.png"  };
+   $scope.u6={ name  : 'Vijay' , contact : '052-5790357' , role : 'Open Stack' ,logo: "img/openstack.jpg"  };
 
-   $scope.m1={ name  : 'Cloud Lab Planning' , when : '19-Nov 10am'  };
-   $scope.m2={ name  : 'Cloud Network Workshop' , when : '13-Nov 10am'  };
+   $scope.m1={ name  : 'Cloud Lab Planning' , when : '17-Dec 10am'  };
+   $scope.m2={ name  : 'Cloud Network Workshop' , when : '28-Dec 10am'  };
    
     $scope.p3= { phasename :'Project  Started' , info : '1-NOV-2015' };
     $scope.p2= { phasename :'Equipment Ordered' , info : '24-NOV-2015' };
